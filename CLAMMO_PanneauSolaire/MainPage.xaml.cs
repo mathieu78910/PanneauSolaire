2d6150ef13534ecbce457c738b78498c1ed1532e
whiteban <mathieu.monnie@campus-la-chataigneraie.org>
2024-10-15 22:00:24 +0200

    rajout d'un titre fonction remplie mais jai pas réussie a afficher les données a voir mercredi et rajout d'une demande pour acceder aux données

--- CLAMMO_PanneauSolaire/MainPage.xaml.cs
+++ CLAMMO_PanneauSolaire/MainPage.xaml.cs
@@ -6,20 +6,64 @@
         {
             InitializeComponent();
         }
-
+        
         private void OnGeolocationClicked(object sender, EventArgs e)
         {
-
+            /*
+            try
+            {
+                var location = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best));
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to get location: {ex.Message}");
+            }
+            */
         }
-
+        
         private void OnCompassClicked(object sender, EventArgs e)
         {
+            if (Compass.IsMonitoring)
+            {
+                Compass.Stop(); 
+                Console.WriteLine("Compass stopped.");
+            }
+            else
+            {
+                Compass.ReadingChanged += (s, e) =>
+                {
+                    var data = e.Reading;
+                    Console.WriteLine($"Heading: {data.HeadingMagneticNorth} degrees");
+                };
+                Compass.Start(SensorSpeed.UI); 
+                Console.WriteLine("Compass started.");
+            }
 
         }
 
         private void OnInclinationClicked(object sender, EventArgs e)
         {
-
+            if (Accelerometer.IsMonitoring)
+            {
+                Accelerometer.Stop(); 
+                Console.WriteLine("Accelerometer stopped.");
+            }
+            else
+            {
+                Accelerometer.ReadingChanged += (s, e) =>
+                {
+                    var data = e.Reading;
+                    
+                    double inclination = Math.Atan2(data.Acceleration.Y, data.Acceleration.Z) * (180 / Math.PI);
+                    Console.WriteLine($"Inclination: {inclination} degrees");
+                };
+                Accelerometer.Start(SensorSpeed.UI); 
+                Console.WriteLine("Accelerometer started.");
+            }
         }
     }
 
--- CLAMMO_PanneauSolaire/Platforms/Android/MainActivity.cs
+++ CLAMMO_PanneauSolaire/Platforms/Android/MainActivity.cs
@@ -1,11 +1,40 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android;
 
 namespace CLAMMO_PanneauSolaire
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Vérifie si la permission d'accès à la localisation a été accordée
+            if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+            {
+                // Si non, demande la permission à l'utilisateur
+                RequestPermissions(new string[] { Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessCoarseLocation }, 0);
+            }
+        }
+
+        // Gérer la réponse de l'utilisateur à la demande de permission
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == 0 && grantResults.Length > 0 && grantResults[0] == Permission.Granted)
+            {
+                // Permission accordée
+                Console.WriteLine("Permission d'accès à la localisation accordée.");
+            }
+            else
+            {
+                // Permission refusée
+                Console.WriteLine("Permission d'accès à la localisation refusée.");
+            }
+        }
     }
 }
